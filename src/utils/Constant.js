9387d8fbbf2d0823febb6f1901e1e967579cf5e5
[tushar khatri] <[tusharkhatri1001@gmail.com]>
2023-10-18 07:56:00 +0530

    first commit

---
+++ src/utils/Constant.js
@@ -0,0 +1,33 @@
+export const PROJECT_DATA = [
+  {
+    position: "right",
+    link: "https://raw.githubusercontent.com/tusharkhatri434/my-project-assets/master/p1.png",
+    title: "Youtube Clone",
+    date: "(AUGUST 2023) 🚀",
+    desc: "Built with React, Tailwind, and Redux, my YouTube clone offers efficient API calls with debouncing search filters, delivering top-notch user experience.",
+    tags: ["tailwind", "React", "Redux","Live Chat"],
+    live: "https://tushar-youtube-app.netlify.app/",
+    github: "https://github.com/tusharkhatri434/my-youtube",
+  },
+  {
+    position: "left",
+    link: "https://raw.githubusercontent.com/tusharkhatri434/my-project-assets/master/p3.png",
+    title: "FoodVilla",
+    date: "(MARCH 2023) 🎪",
+    desc: "FoodVill web app, created with React, Tailwind, Redux Toolkit, and Jest testing, fully responsive streamlines online business with API-driven search filters and cart functionality.",
+
+    tags: ["tailwind", "React", "Redux","React-Router-dom"],
+    live: "https://food-villa1.netlify.app/",
+    github: "https://github.com/tusharkhatri434/food-villa",
+  },
+  {
+    position: "right",
+    link: "https://raw.githubusercontent.com/tusharkhatri434/my-project-assets/master/p4.png",
+    title: "To-DO App",
+    date: "(FEBRUARY 2023) 📲",
+    desc: "My to-do app, designed with pure HTML, CSS, and JS, offers full CRUD functionality and responsive design for seamless task management.",
+    tags: ["Html", "CSS3", "JavaScript"],
+    live: "https://to-do-app-tushar-khatri.netlify.app/",
+    github: "https://github.com/tusharkhatri434/To-do_App",
+  },
+];
